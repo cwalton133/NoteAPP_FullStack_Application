8d75a5d39552e21187eab5db66a814566d8c3500
Charles Walton <164152127+cwalton133@users.noreply.github.com>
2025-02-20 03:55:18 -0800

    Update settings.py

--- noteapp_backend/noteapp_backend/settings.py
+++ noteapp_backend/noteapp_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1#$y2wqo_+6ea92hn*l+-v2=lx6vtqu_4r1lh4yubmr7r$i^a0"
+SECRET_KEY = " "
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
